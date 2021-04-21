3d500fae9273f6264de71c4789e437610c0ab70d
llleyelll <chida.fua@gmail.com>
2021-04-21 15:03:18 +0700

    initial project

---
+++ Facio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facio
+//
+//  Created by Chananchida Fuachai on 21/4/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facio/FacioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacioApp.swift
+//  Facio
+//
+//  Created by Chananchida Fuachai on 21/4/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct FacioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
