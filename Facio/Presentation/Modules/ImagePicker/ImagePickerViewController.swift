af615c8b1bfca8f79cdcb0c00922256c74a34e83
Sirikonss <macintoshhd@Macintoshs-MacBook-Pro.local>
2022-01-17 11:42:10 +0700

    init image picker

---
+++ Facio/Presentation/Modules/ImagePicker/ImagePickerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePickerViewController.swift
+//  Facio
+//
+//  Created by Sirikonss on 16/1/2565 BE.
+//
+
+import SwiftUI
+
+struct ImagePickerViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePickerViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePickerViewController()
+    }
+}
