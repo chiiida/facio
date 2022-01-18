28b8dac6619b1e253853109997dda366dc07bc42
Sirikonss <macintoshhd@Macintoshs-MacBook-Pro.local>
2022-01-18 12:05:45 +0700

    Update fix presenting

--- Facio/Presentation/Modules/ImagePicker/ImagePickerViewController.swift
+++ Facio/Presentation/Modules/ImagePicker/ImagePickerViewController.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 final class ImagePickerViewController: UIImagePickerController {
-
-    private let cancelButton = UIButton(type: .system)
     
     override func viewDidLoad() {
             super.viewDidLoad()
@@ -23,22 +21,6 @@ extension ImagePickerViewController {
         navigationController?.navigationItem.hidesBackButton = true
         sourceType = .photoLibrary
         allowsEditing = true
-        setUpHeader()
-        setUpNavigationBar()
        }
     
-    private func setUpHeader() {
-        cancelButton.setTitle("Cancel", for: .normal)
-        cancelButton.addTarget(self, action: #selector(didTapCancelButton), for: .touchUpInside)
-    }
-    
-    private func setUpNavigationBar() {
-        navigationItem.leftBarButtonItems = [
-            UIBarButtonItem(customView: cancelButton)
-        ]
-    }
-    
-    @objc private func didTapCancelButton() {
-        navigationController?.dismiss(animated: true, completion: nil)
-    }
 }
