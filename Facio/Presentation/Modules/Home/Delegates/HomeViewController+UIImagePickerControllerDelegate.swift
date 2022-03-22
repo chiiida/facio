52528ab990346887249fd5bdd3cc4463c562d01d
chiiida <chida.fua@gmail.com>
2022-03-22 15:25:55 +0700

    Add missing function

--- Facio/Presentation/Modules/Home/Delegates/HomeViewController+UIImagePickerControllerDelegate.swift
+++ Facio/Presentation/Modules/Home/Delegates/HomeViewController+UIImagePickerControllerDelegate.swift
@@ -8,17 +8,34 @@
 import UIKit
 
 extension HomeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func fixOrientation(img: UIImage) -> UIImage {
+        if img.imageOrientation == .up {
+            return img
+        }
+
+        UIGraphicsBeginImageContextWithOptions(img.size, false, img.scale)
+        let rect = CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height)
+        img.draw(in: rect)
+
+        guard let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        else { return img }
+        UIGraphicsEndImageContext()
+
+        return normalizedImage
+    }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         guard let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage ??
                 info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
 
+        let imageToDisplay = fixOrientation(img: pickedImage)
         let faceNode = FaceNode(at: FeatureIndices.nose)
         let timestamp = Date().timeIntervalSince1970
         let imageNodeName = "image\(timestamp)"
         faceNode.name = imageNodeName
         let viewModel = FaceNodeViewModel(node: faceNode)
-        viewModel.addImage(pickedImage)
+        viewModel.addImage(imageToDisplay)
         arView.addNode(from: viewModel)
         dismiss(animated: true, completion: nil)
     }
