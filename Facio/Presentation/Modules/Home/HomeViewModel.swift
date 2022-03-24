e0c2a7fe701ffc1b10768aa415b83bd9a980005b
chiiida <chida.fua@gmail.com>
2022-03-24 11:44:55 +0700

    Remove comments

--- Facio/Presentation/Modules/Home/HomeViewModel.swift
+++ Facio/Presentation/Modules/Home/HomeViewModel.swift
@@ -5,18 +5,10 @@
 //  Created by Chananchida Fuachai on 13/1/2565 BE.
 //
 
-protocol HomeViewModelInput {
-
-//    func didTapSettingsButton()
-}
+protocol HomeViewModelInput {}
 
 protocol HomeViewModelOutput {}
 
 protocol HomeViewModelProtocol: HomeViewModelInput, HomeViewModelOutput {}
 
-final class HomeViewModel: HomeViewModelProtocol {
-
-//    func didTapSettingsButton() {
-//        // TODO: implement in integration
-//    }
-}
+final class HomeViewModel: HomeViewModelProtocol {}
